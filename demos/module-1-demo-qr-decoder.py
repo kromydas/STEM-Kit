99111c5e6d6ae887f4fdd48503d2dd657b68cc94
Bill Kromydas <bkromydas@gmail.com>
2023-05-04 11:30:43 -0700

    Changed algorithm to pyzbar. Changed centroid to use bbox. Add capability to set initial window location.

--- demos/module-1-demo-qr-decoder.py
+++ demos/module-1-demo-qr-decoder.py
@@ -19,23 +19,34 @@
 import argparse
 import cv2
 import numpy as np
+import pyzbar.pyzbar as pyzbar
 
 # Set font scale and thickness for STEM-Kit resolution.
 font_scale = .8
 font_thickness = 2
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--scale', '-sc', type=float, default=.5, help='Scale factor used to resize input video frames.')
+parser.add_argument('--scale', '-sc', type=float, default=.75, help='Scale factor used to resize input video frames.')
 args = parser.parse_args()
 
-def find_centroid_vertices(points):
+# def find_centroid_vertices(points):
+#     '''
+#     Finds the centroid of the input coordinates.
+#     '''
+#     min_x, max_x = np.min(points[:, 0]), np.max(points[:, 0])
+#     min_y, max_y = np.min(points[:, 1]), np.max(points[:, 1])
+#     mid_x = (min_x + max_x) / 2
+#     mid_y = (min_y + max_y) / 2
+#
+#     # Return the coordinates of the centroid.
+#     return mid_x, mid_y
+
+def find_centroid_bbox(bbox):
     '''
-    Finds the centroid of the input coordinates.
+    Finds the centroid of the input bounding box [x, y, w, h].
     '''
-    min_x, max_x = np.min(points[:, 0]), np.max(points[:, 0])
-    min_y, max_y = np.min(points[:, 1]), np.max(points[:, 1])
-    mid_x = (min_x + max_x) / 2
-    mid_y = (min_y + max_y) / 2
+    mid_x = int(bbox[0] + bbox[2] / 2)
+    mid_y = int(bbox[1] + bbox[3] / 2)
 
     # Return the coordinates of the centroid.
     return mid_x, mid_y
@@ -72,6 +83,8 @@ frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) * args.scale)
 # Initialize QR detector.
 qcd = cv2.QRCodeDetector()
 
+move_window_flag = True
+
 # Enter infinite loop to process an endless video stream from the connected camera.
 while(True):
 
@@ -84,21 +97,30 @@ while(True):
 
         # Convert the current frame to grayscale and decode any found QR codes.
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        found_qr_code, decoded_info, points, straight_qrcode = qcd.detectAndDecodeMulti(gray)
+        # found_qr_code, decoded_info, points, straight_qrcode = qcd.detectAndDecodeMulti(gray)
+        decoded = pyzbar.decode(gray)
 
-        if found_qr_code is True:
+        for code in decoded:
 
-            # Outline each detected QR code.
-            frame = cv2.polylines(frame, points.astype(int), True, (0, 255, 0), thickness=2)
+            # Extract bounding box location and size.
+            bbox = [x, y, w, h] = code.rect
 
-            # Annotate the frame with the decoded message.
-            for msg, points in zip(decoded_info, points):
-                centroid = find_centroid_vertices(points)
-                draw_label_banner(frame, msg, centroid, font_color=(255, 255, 255), fill_color=(255, 0, 0), font_scale=font_scale,
-                                  font_thickness=font_thickness)
+            # Draw bounding box rectangle around the QR code
+            cv2.polylines(frame, [np.array(code.polygon)], True, (0, 255, 0), 2)
+
+            # Get decoded text from the QR code
+            msg = code.data.decode('utf-8')
+
+            centroid = find_centroid_bbox(bbox)
+            draw_label_banner(frame, msg, centroid, font_color=(255, 255, 255), fill_color=(255, 0, 0), font_scale=font_scale,
+                                      font_thickness=font_thickness)
 
         # Display annotated frame in window.
         cv2.imshow('Live', frame)
+
+        if move_window_flag:
+            cv2.moveWindow('Live', 170, 20)
+            move_window_flag = False
 
         key = cv2.waitKey(1)
         # Quit program when `q` or the `esc` key is selected.
