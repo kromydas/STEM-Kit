9290f6c8590a3413a1342b9a9f66efa6e11dc55d
Bill Kromydas <bkromydas@gmail.com>
2023-05-15 18:42:30 -0700

    Added OCR-Translation App to GUI.

--- STEM-kit.py
+++ STEM-kit.py
@@ -15,6 +15,7 @@ import numpy as np
 import cv2
 #import pyzbar.pyzbar as pyzbar
 import onnxruntime
+import googletrans
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 
@@ -135,8 +136,8 @@ class MainLayout(BoxLayout):
             "Edge Detection",
             "Image Deblurring",
             "Face Recognition",
+            "OCR + Translation",
             "QR Code Decoder",
-            "Module 5",
             "Module 6",
             "Module 7",
             "Module 8",
@@ -175,6 +176,8 @@ class MainLayout(BoxLayout):
             popup = DeblurringPopup(self)
         elif module_name == "Edge Detection":
             popup = EdgeDetectionPopup(self)
+        elif module_name == "OCR + Translation":
+            popup = OCRTranslationPopup(self)
         else:
             # Add additional modules here
             popup = UnderConstructionPopup(self)
@@ -220,7 +223,8 @@ class BasePopup(Popup):
 
     def create_base_widgets(self):
         self.content = BoxLayout(orientation="vertical")
-        self.add_widget(self.content)
+        # self.add_widget(self.content)
+        self.add(self.content)
 
         self.image = Image(allow_stretch=True, size_hint_y=0.7)
         self.content.add_widget(self.image)
@@ -661,6 +665,149 @@ class EdgeDetectionPopup(BasePopup):
     def apply_canny_edge_detection(img, lower_threshold, upper_threshold):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         return cv2.Canny(gray, lower_threshold, upper_threshold)
+
+class OCRTranslationPopup(BasePopup):
+    def __init__(self, main_layout, **kwargs):
+        super(OCRTranslationPopup, self).__init__(main_layout, **kwargs)
+        self.title = "OCR + Translation"
+
+        self.content = BoxLayout(orientation="vertical", spacing=layout_padding_y)
+
+        self.image = Image(allow_stretch=True, size_hint_y=0.7)
+        self.content.add_widget(self.image)
+
+        button_layout = BoxLayout(size_hint_y=0.1)
+        self.content.add_widget(button_layout)
+
+        self.close_button = Button(text="Close")
+        self.close_button.bind(on_press=self.close_popup)
+        button_layout.add_widget(self.close_button)
+
+        # Create a Translator Object.
+        self.translator = googletrans.Translator()
+
+        vocabulary = []
+
+        # Open file to import the vocabulary.
+        with open("./models/alphabet_94.txt") as f:
+            # Read the file line by line
+            for l in f:
+                # Append each line into the vocabulary list.
+                vocabulary.append(l.strip())
+            f.close()
+        print("Vocabulary:", vocabulary, len(vocabulary))
+
+        # DB model for text-detection based on resnet50
+        self.textDetector = cv2.dnn_TextDetectionModel_DB("./models/DB_TD500_resnet50.onnx")
+
+        # Set threshold for Binary Map creation and polygon detection
+        binThresh = 0.3
+        polyThresh = 0.5
+
+        mean = (122.67891434, 116.66876762, 104.00698793)
+        inputSize = (640, 640)
+
+        self.textDetector.setBinaryThreshold(binThresh).setPolygonThreshold(polyThresh)
+        self.textDetector.setInputParams(1.0 / 255, inputSize, mean, True)
+
+        # CRNN model for text-recognition.
+        self.textRecognizer = cv2.dnn_TextRecognitionModel("./models/crnn_cs.onnx")
+        self.textRecognizer.setDecodeType("CTC-greedy")
+        self.textRecognizer.setVocabulary(vocabulary)
+        self.textRecognizer.setInputParams(1 / 127.5, (100, 32), (127.5, 127.5, 127.5), True)
+
+        Clock.schedule_interval(self.process_image, 1.0 / 30.0)
+
+        self.frame_count = 0
+
+    def draw_label_banner_ocr(self, frame, text, lower_left, font_color=(0, 0, 0), fill_color=(255, 255, 255), font_scale=1,
+                              font_thickness=1):
+        '''
+        Annotate the image frame with a text banner overlayed on a filled rectangle.
+        '''
+        text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, font_scale, font_thickness + 2)[0]
+        text_pad = 8
+
+        # Define upper left and lower right vertices of rectangle.
+        px1 = lower_left[0] - text_pad
+        px2 = lower_left[0] + int(text_size[0]) + text_pad
+        py1 = lower_left[1] - int(text_size[1]) - text_pad
+        py2 = lower_left[1] + text_pad
+
+        frame = cv2.rectangle(frame, (px1, py1), (px2, py2), fill_color, thickness=-1, lineType=cv2.LINE_8)
+
+        cv2.putText(frame, text, lower_left, cv2.FONT_HERSHEY_SIMPLEX, font_scale, font_color, font_thickness,
+                    cv2.LINE_AA)
+
+    # Align text boxes.
+    # This Function does transformation over the bounding boxes detected by the text detection model
+    def fourPointsTransform(self, frame, vertices):
+        # Print vertices of each bounding box
+        vertices = np.asarray(vertices).astype(np.float32)
+        outputSize = (100, 32)
+        targetVertices = np.array([
+            [0, outputSize[1] - 1],
+            [0, 0],
+            [outputSize[0] - 1, 0],
+            [outputSize[0] - 1, outputSize[1] - 1]], dtype="float32")
+        # Apply perspective transform
+        rotationMatrix = cv2.getPerspectiveTransform(vertices, targetVertices)
+        result = cv2.warpPerspective(frame, rotationMatrix, outputSize)
+        return result
+
+    # Perform Language Translation on Recognized Text.
+    def recognizeTranslateText(self, image, dest='en', src=''):
+
+        # Use the DB text detector initialized previously to detect the presence of text in the image.
+        boxes, confs = self.textDetector.detect(image)
+
+        # Iterate throught the bounding boxes detected by the text detector model
+        for box in boxes:
+
+            # Apply transformation on the bounding box detected by the text detection algorithm.
+            croppedRoi = self.fourPointsTransform(image, box)
+
+            # Recognize the text using the crnn model.
+            recognizedText = self.textRecognizer.recognize(croppedRoi)
+
+            if recognizedText is not None and recognizedText.strip() != '':
+                # translation = translator.translate(recognizedText, dest, src)
+                if src:
+                    translation = self.translator.translate(recognizedText, dest, src)
+                else:
+                    translation = self.translator.translate(recognizedText, dest)
+            else:
+                print("No text was recognized in the frame.")
+
+            pad_x = 10
+            shift_y = 10
+            px = int(np.max(box[0:4, 0])) + pad_x
+            py = int(np.average(box[0:4, 1])) + shift_y
+
+            lower_left = (px, py)
+
+            self.draw_label_banner_ocr(image, translation.text, lower_left, font_color=(255, 255, 255),
+                              fill_color=(255, 0, 0), font_scale=0.7, font_thickness=2)
+
+        # Draw the bounding boxes of text detected.
+        cv2.polylines(image, boxes, True, (255, 0, 255), 3)
+
+        return image
+
+    def process_image(self, instance):
+        Clock.schedule_once(self.process_image_thread, 0)
+
+    def process_image_thread(self, dt, *args):
+
+        frame = self.get_latest_frame()
+        if frame is None:
+            return
+        else:
+            # ocr_result = recognizeTranslateText(frame, src='de')
+            frame = self.recognizeTranslateText(frame)
+
+            texture = self.convert_frame_to_texture(self, frame)
+            self.image.texture = texture
 
 
 class UnderConstructionPopup(BasePopup):
