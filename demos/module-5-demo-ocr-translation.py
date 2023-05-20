20c5ef2bc27b7fdb6b44bdbfc877af631aab8d89
Bill Kromydas <bkromydas@gmail.com>
2023-05-20 07:58:29 -0700

    Code clean up and documentation.

--- demos/module-5-demo-ocr-translation.py
+++ demos/module-5-demo-ocr-translation.py
@@ -9,10 +9,19 @@
 #
 #    ~/demos $ python module-5-demo-ocr-translation.py
 #
-# A window will first appear that displays the input image (foreign-language.png).
+# A window will first appear that displays the input image (foreign_text.png).
 # Select any key on the keyboard to initiate the processing and a new window will
 # be displayed that shows the detected text and the associated translation to
 # English.
+#
+# Alternatively, the script can be executed with an optional command line argument
+# specify a different input file:
+#
+#    ~/demos $ python module-5-demo-ocr-translation.py --image
+#
+# Note: Since this script makes calls the Google Translate API, an internet
+# connection is required. If the script fails during processing this may
+# be due to exceeding API limits from a given device.
 #
 # Developed by Big Vision LLC for Emerging Technologies Institute (ETI).
 #------------------------------------------------------------------------------
@@ -23,17 +32,22 @@ import googletrans
 import argparse
 
 ap= argparse.ArgumentParser()
-ap.add_argument('--image', '-i', default='./foreign_text.png', help='Path to input image that contains foreign text.')
+ap.add_argument('--image', '-i', default='./foreign_text.png', help='Path to input image that contains foriegn text.')
 args= vars(ap.parse_args())
 
-
-def draw_label_banner_ocr(frame, text, lower_left, font_color=(0, 0, 0), fill_color=(255, 255, 255), font_scale=1,
-                          font_thickness=1):
+def draw_label_banner_ocr(frame, text, lower_left, font_color=(0, 0, 0), fill_color=(255, 255, 255), font_scale=1, font_thickness=1):
     """
-    Annotate the image frame with a text banner overlayed on a filled rectangle.
+    Annotate the image frame with a text banner overlaid on a filled rectangle.
+    :param frame: Input image frame.
+    :param text: Text string to annotate on frame.
+    :param lower_left: (x,y) coordinates for the lower-left corner of the text block.
+    :param font_color: Font color for the annotaed text string.
+    :param fill_color: Fill color for the background rectangle.
+    :param font_scale: Font scale for the annotated text.
+    :param font_thickness: Font thickness for the annotated text.
     """
     text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, font_scale, font_thickness + 2)[0]
-    text_pad = 8
+    text_pad = 8 # Number of pixels used to enlarge background filled rectangle.
 
     # Define upper left and lower right vertices of rectangle.
     px1 = lower_left[0] - text_pad
@@ -41,13 +55,19 @@ def draw_label_banner_ocr(frame, text, lower_left, font_color=(0, 0, 0), fill_co
     py1 = lower_left[1] - int(text_size[1]) - text_pad
     py2 = lower_left[1] + text_pad
 
+    # Annotate frame with filled rectangle.
     frame = cv2.rectangle(frame, (px1, py1), (px2, py2), fill_color, thickness=-1, lineType=cv2.LINE_8)
 
-    cv2.putText(frame, text, lower_left, cv2.FONT_HERSHEY_SIMPLEX, font_scale, font_color, font_thickness,
-                cv2.LINE_AA)
+    # Annotate frame with text.
+    cv2.putText(frame, text, lower_left, cv2.FONT_HERSHEY_SIMPLEX, font_scale, font_color, font_thickness, cv2.LINE_AA)
 
-# This function performs a transformation for each bounding box detected by the text detection model.
 def fourPointsTransform(frame, vertices):
+    """
+    # This function performs a transformation for each bounding box detected by the text detection model.
+    :param frame: Input image frame with detected text.
+    :param vertices: Verticies of polygon for detected text block.
+    :return: Annotated image frame.
+    """
 
     # Print vertices of each bounding box.
     vertices = np.asarray(vertices).astype(np.float32)
@@ -63,9 +83,14 @@ def fourPointsTransform(frame, vertices):
     result = cv2.warpPerspective(frame, rotationMatrix, outputSize)
     return result
 
-# Perform Language Translation on Recognized Text.
 def recognizeTranslateText(image, dest='en', src=''):
-
+    """
+    # Perform Language Translation on Recognized Text.
+    :param image: Input image frame.
+    :param dest: Destination langauge (default is English).
+    :param src: Source langauge (default: unspecified)
+    :return: Annotated image frame with text bounding boxes and translated text.
+    """
     # Use the DB text detector initialized previously to detect the presence of text in the image.
     boxes, confs = textDetector.detect(image)
 
@@ -79,7 +104,6 @@ def recognizeTranslateText(image, dest='en', src=''):
         recognizedText = textRecognizer.recognize(croppedRoi)
 
         if recognizedText is not None and recognizedText.strip() != '':
-            # translation = translator.translate(recognizedText, dest, src)
             if src:
                 translation = translator.translate(recognizedText, dest, src)
             else:
@@ -87,11 +111,11 @@ def recognizeTranslateText(image, dest='en', src=''):
         else:
             print("No text was recognized in the frame.")
 
+        # Coordinates text annotation.
         pad_x = 10
         shift_y = 10
         px = int(np.max(box[0:4,0])) + pad_x
         py = int(np.average(box[0:4,1])) + shift_y
-
         lower_left = (px, py)
 
         draw_label_banner_ocr(image, translation.text, lower_left, font_color=(255, 255, 255),
@@ -103,7 +127,8 @@ def recognizeTranslateText(image, dest='en', src=''):
     return image
 
 if __name__ == "__main__":
-    print(googletrans.LANGUAGES)
+
+    #print(googletrans.LANGUAGES)
 
     # Create a Translator Object.
     translator = googletrans.Translator()
@@ -111,43 +136,43 @@ if __name__ == "__main__":
     vocabulary = []
     try:
         with open("../models/alphabet_94.txt") as f:
-            # Read the file line by line
+            # Read the file line by line.
             for l in f:
                 # Append each line into the vocabulary list.
                 vocabulary.append(l.strip())
     except FileNotFoundError:
         print("File not found!")
-        # Handle error appropriately, maybe exit the program
+        exit()
     except PermissionError:
         print("No permission to read the file!")
-        # Handle error appropriately
+        exit()
 
     try:
-        # DB model for text-detection based on resnet50
+        # DB model for text-detection based on resnet50.
         textDetector = cv2.dnn_TextDetectionModel_DB("../models/DB_TD500_resnet50.onnx")
     except FileNotFoundError:
         print("File not found!")
-        # Handle error appropriately, maybe exit the program
+        exit()
     except PermissionError:
         print("No permission to read the file!")
-        # Handle error appropriately
+        exit()
 
     try:
         # CRNN model for text-recognition.
         textRecognizer = cv2.dnn_TextRecognitionModel("../models/crnn_cs.onnx")
     except FileNotFoundError:
         print("File not found!")
-        # Handle error appropriately, maybe exit the program
+        exit()
     except PermissionError:
         print("No permission to read the file!")
-        # Handle error appropriately
+        exit()
 
-    # Set threshold for Binary Map creation and polygon detection
+    # Set threshold for Binary Map creation and polygon detection.
     binThresh = 0.3
     polyThresh = 0.5
 
     mean = (122.67891434, 116.66876762, 104.00698793)
-    inputSize = (640, 640)
+    inputSize = (480, 480)
 
     textDetector.setBinaryThreshold(binThresh).setPolygonThreshold(polyThresh)
     textDetector.setInputParams(1.0/255, inputSize, mean, True)
@@ -158,23 +183,16 @@ if __name__ == "__main__":
     textRecognizer.setVocabulary(vocabulary)
     textRecognizer.setInputParams(1/127.5, (100,32), (127.5, 127.5, 127.5), True)
 
-    # Set the desired frame size.
-    frameWidth = 512
-    frameHeight = 512
-
     source = args['image']
     ip_image = cv2.imread(source)
 
     frame = ip_image.copy()
-    frame = cv2.resize(frame, (frameWidth, frameHeight))
+    frame = cv2.resize(frame, inputSize)
 
     cv2.imshow("Input", frame)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    # frame = cv2.resize(frame, (frameWidth, frameHeight))
-
-    # ocr_result = recognizeTranslateText(frame, src='de')
     ocr_result = recognizeTranslateText(frame)
 
     cv2.imshow('OCR Translation Result', ocr_result)
