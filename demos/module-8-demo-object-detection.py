915947cec557fde14e6ca8b2ba60162026c00ff3
Bill Kromydas <bkromydas@gmail.com>
2023-05-22 16:24:22 -0700

    Removed dormant code for computing infernece FPS.

--- demos/module-8-demo-object-detection.py
+++ demos/module-8-demo-object-detection.py
@@ -104,14 +104,6 @@ def run(model: str, camera_id: int, width: int, height: int,  num_threads: int, 
   cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
   cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-  # Visualization parameters.
-  # row_size = 20  # pixels
-  # left_margin = 24  # pixels
-  # text_color = (0, 0, 255)  # red
-  # font_size = 1.5
-  # font_thickness = 2
-  # fps_avg_frame_count = 10
-
   # Initialize the object detection model.
   base_options = core.BaseOptions(file_name=model, use_coral=False, num_threads=num_threads)
   detection_options = processor.DetectionOptions(max_results=3, score_threshold=0.3)
@@ -138,18 +130,6 @@ def run(model: str, camera_id: int, width: int, height: int,  num_threads: int, 
 
     # Draw keypoints and edges on input image
     image = visualize(image, detection_result)
-
-    # # Calculate the FPS
-    # if counter % fps_avg_frame_count == 0:
-    #   end_time = time.time()
-    #   fps = fps_avg_frame_count / (end_time - start_time)
-    #   start_time = time.time()
-
-    # # Show the FPS
-    # fps_text = 'FPS = {:.1f}'.format(fps)
-    # text_location = (left_margin, row_size)
-    # cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-    #             font_size, text_color, font_thickness)
 
     key = cv2.waitKey(1)
     # Quit program when `q` or the `esc` key is selected.
